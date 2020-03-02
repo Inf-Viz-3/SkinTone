8337147796a6faff55cd9eeb7e503b8bdbc3c164
Leonard Herold <git@hdld.me>
2020-03-02 23:04:34 +0100

    Improve download

--- facial_landmarks/download.py
+++ facial_landmarks/download.py
@@ -1,21 +1,26 @@
 import os
 import pandas as pd
 import concurrent.futures
-import urllib.request
+import multiprocessing
+import urllib.parse
+import mimetypes
+import requests
 
-xlsx_data = pd.read_excel(os.path.join("..", "met.xlsx"))
-df = pd.DataFrame(xlsx_data, columns= ['id', 'image_url'])
+# use read.py to prepare portrait.csv
+omniart_df = pd.read_csv("omniart_v3_portrait.csv", encoding = 'utf8')
 
 def process_row(rowobj):
     row_data = rowobj[1]
-    if rowobj[0] % 1000 == 0:
-        print("a thousand processed") 
-    filename, file_extension = os.path.splitext(row_data['image_url'])
-    urllib.request.urlretrieve(row_data['image_url'], "imgs/{0}{1}".format(row_data["id"], file_extension))   
+    image_url = urllib.parse.urlparse(row_data['image_url'])
+    filename, file_extension = os.path.splitext(image_url.geturl())
+    image = requests.get(image_url.geturl())
+    with open(f"imgs/{row_data.id}{file_extension}", 'wb') as fd:
+        for chunk in image.iter_content(chunk_size=128):
+            fd.write(chunk)
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
     no_downloaded = 0
-    futures = { executor.submit(process_row, row,): row for row in df.iterrows() }
+    futures = { executor.submit(process_row, row,): row for row in omniart_df.iterrows() }
     for future in concurrent.futures.as_completed(futures):
         result = futures[future]
         try:

e69859ff43a1d525e9d13f382bde02ee49a94b31
Leonard Herold <git@hdld.me>
2020-03-03 14:45:15 +0100

    Add age and gender detection

--- facial_landmarks/faces_detect.py
+++ facial_landmarks/faces_detect.py
@@ -12,13 +12,89 @@ import multiprocessing
 
 import face_average
 
+def getFaceBox(net, frame, conf_threshold=0.7):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
+def scan(frame):
+
+    # Load network
+    faceProto = "opencv_face_detector.pbtxt"
+    faceModel = "opencv_face_detector_uint8.pb"
+
+    ageProto = "age_deploy.prototxt"
+    ageModel = "age_net.caffemodel"
+
+    genderProto = "gender_deploy.prototxt"
+    genderModel = "gender_net.caffemodel"
+    ageNet = cv2.dnn.readNet(ageModel, ageProto)
+    genderNet = cv2.dnn.readNet(genderModel, genderProto)
+    faceNet = cv2.dnn.readNet(faceModel, faceProto)
+
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    genderList = ['Male', 'Female']
+    
+    padding = 20
+
+    frameFace, bboxes = getFaceBox(faceNet, frame)
+    if not bboxes:
+        # print(f"\t No face Detected, Checking next frame")
+        return [], []
+
+    gender_ = []
+    age_ = []
+    for bbox in bboxes:
+        #print(f'\t Face Detected')
+        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+
+        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        gender_.append(gender)
+        
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
+        age_.append(age)
+
+    return gender_, age_
+
 def process_image(filename):
     basename, file_extension = os.path.splitext(os.path.basename(filename))
     
     imgcv = cv2.imread(filename, cv2.IMREAD_COLOR)
 
     dets = detector(imgcv, 1)
+
+    
+    if (len(dets) == 0):
+        cv2.imwrite("missed/{0}.png".format(basename), imgcv)
+        return None
+
     imgfaces_df = pd.DataFrame()
+    faces_gender, faces_age = scan(imgcv)
+
+    if len(faces_gender) != len(dets):
+        return None
+
     for k, d in enumerate(dets):
         # Get the landmarks/parts for the face in box d.
         crop = imgcv[d.top():d.bottom(), d.left():d.right()]
@@ -33,14 +109,13 @@ def process_image(filename):
             'imgid': [basename],
             'faceid': [k],
             'box': [[d.top(), d.bottom(),d.left(), d.right()]],
-            'points': [points]
+            'points': [points],
+            "gender": faces_gender[k],
+            "age": faces_age[k]
         })
         imgfaces_df = imgfaces_df.append(df)
+        print("img {0} - {1} {2}".format(len(dets), len(faces_gender), len(faces_age)))
     
-        
-    if (imgfaces_df.count == 0):
-        cv2.imwrite("missed/{0}.png".format(basename), imgcv)
-        return None
     return imgfaces_df
 
 def process_pipeline(filename):
